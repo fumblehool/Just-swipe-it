57b4cff3163d9c8cbd4f2270700fc9975725a6ec
Damanpreet <daman.4880@gmail.com>
2016-05-12 17:35:21 +0530

    Added basic files

---
+++ Just-swipe-it/app/src/main/java/com/fumblehool/daman/just_swipe_it/MainActivity.java
@@ -0,0 +1,13 @@
+package com.fumblehool.daman.just_swipe_it;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
